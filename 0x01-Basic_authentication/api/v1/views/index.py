e91e48fd99f60e2b548621268230d5b1ba16a3dd
“victornnamdii” <victorilodiuba@gmail.com>
2023-03-06 23:48:30 +0100

    Task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def request_unauthorized() -> None:
+    """
+    Testing 401 error handler
+    """
+    abort(401)
