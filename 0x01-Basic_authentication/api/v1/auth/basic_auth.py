de818c177eaf7a70ee944762659669158956814c
“victornnamdii” <victorilodiuba@gmail.com>
2023-03-07 12:08:46 +0100

    Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,13 @@ class BasicAuth(Auth):
     """
     Basic Authentication class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header for a Basic
+        Authentication
+        """
+        if authorization_header and isinstance(authorization_header, str):
+            if authorization_header.startswith('Basic '):
+                return authorization_header[6:]
+        return None
