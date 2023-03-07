244853fc902ab8b9ede0abe1ff724379556ae72a
“victornnamdii” <victorilodiuba@gmail.com>
2023-03-07 12:30:51 +0100

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic Authentication class
 """
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+import binascii
 
 
 class BasicAuth(Auth):
@@ -16,7 +18,21 @@ class BasicAuth(Auth):
         Returns the Base64 part of the Authorization header for a Basic
         Authentication
         """
-        if authorization_header and isinstance(authorization_header, str):
-            if authorization_header.startswith('Basic '):
-                return authorization_header[6:]
+        if isinstance(authorization_header, str) and\
+                authorization_header.startswith('Basic '):
+            return authorization_header[6:]
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        try:
+            assert (isinstance(base64_authorization_header, str))
+            string = b64decode(base64_authorization_header, validate=True)
+            string = string.decode('utf-8')
+            return string
+        except (binascii.Error, AssertionError):
+            return None
