8b6b70dd609f9fc2482ef5a80ce3c78fcdf7c1a1
“victornnamdii” <victorilodiuba@gmail.com>
2023-03-14 23:06:13 +0100

    Task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home() -> str:
+    """
+    Home Page
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
