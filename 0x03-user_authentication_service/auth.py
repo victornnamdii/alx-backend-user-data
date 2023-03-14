b19dca5ff8bbfb787dd1eb30e5f554068de33e80
“victornnamdii” <victorilodiuba@gmail.com>
2023-03-14 21:39:46 +0100

    Task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,9 @@ Handling Authorization
 """
 
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +14,23 @@ def _hash_password(password: str) -> bytes:
     Retruns a salted hash of the input password
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Hashes the password of a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(user.email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
